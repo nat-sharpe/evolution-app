1df14e34722cda72d6bdea3f3f16a1102e34ad71
Nat Sharpe <sharpe1890@gmail.com>
2019-01-27 11:31:58 -0500

    First element on canvas

--- canvas.js
+++ canvas.js
@@ -1,1 +1,13 @@
-console.log("You are the creator")
+// Connects to canvas in index.html
+let canvas = document.querySelector('canvas');
+
+// Sets canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Creates a context 'super object'
+let c = canvas.getContext('2d');
+
+// Sets coordinates and dimensions of c: (x, y, width, height)
+// Coordinates are measured from top-left corner of screen (0, 0)
+c.fillRect(100, 100, 100, 100);
